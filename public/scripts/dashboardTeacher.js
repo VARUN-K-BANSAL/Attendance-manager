9c450d836695fe05bf97b313f52380912792f538
Varun Kumar Bansal <varunbansal326@gmail.com>
2022-04-22 15:48:38 +0530

    removed addClass.js and on teachers dashboard teachers can see on ly those classes in which they are involved

--- public/scripts/dashboardTeacher.js
+++ public/scripts/dashboardTeacher.js
@@ -52,37 +52,109 @@ function closeAddClassModal() {
 
 let stdSet = new Set();
 let i = 0;
-
-// let attendInpEl = document.getElementById("attendanceInput");
-// let attendBtnEl = document.querySelector(".attendance_btn");
-
-
 let addStudentAttendance = () => {
 
     let attendInpEl = document.getElementById("attendanceInput");
-
     let stdStr = "";
     stdSet.forEach((ele) => {
         stdStr += `${ele};;`;
     })
-
-    // console.log("Click !!")
-
-    // console.log(`stdStr = ${stdStr}`);
-
-    // console.log(attendInpEl)
-
     attendInpEl.value = stdStr;
-
-    // console.log(`input field = ${attendInpEl.value}`);
-
 }
+
+let classTemplate = (name) => {
+    return `<div class="class">
+    <div class = "class_name" id = "${name}">
+        <h3>${name}</h3>
+    </div>                    
+    <div class = "function_links">
+        <button class = "btn ${name}" id="scanQrCodeBtn" style="border: none; outline: none;" onclick="showScanQrCode()">Scan QR Code</button>
+        <a href="/showAttendance/${name}" class = "btn" target="__blank">Show Attendance</a>
+        <a href="/generateQrCode/${name}" class = "btn">Generate QR</a>
+        <button href="#" class="btn ${name}" id="addStudentBtn" style="border: none; outline: none;" onclick="addStudent()">Add Student</button>
+        <button href="#" class="btn ${name}" id="addTeacherBtn" style="border: none; outline: none;" onclick="addTeacher()">Add Teacher</button>
+        <a href="/removeClass/${name}" class = "btn">Remove Class</a>
+    </div>
+
+    <div class="modal" id="qrCodeScannerModal">
+        <div class="modal-content modal-animate">
+            <a class="close" onclick="closeScanningModal()">&times;</a>
+            <div class="row">
+                <div class="col">
+                    <div id="reader"></div>
+                </div>
+                <div class="col" style="padding:3rem;">
+                    <h4 style = "margin-bottom: 2rem;">SCAN RESULT</h4>
+                    <div id="result">Result Here</div>
+                    
+                    <form action="/markAttendance/${name}" class = "attendanceForm" method = "post">
+                        <input id = "attendanceInput" type="text" name = "qrCodeArr" style = "display: none;">
+                        <button type="submit" class = "attendance_btn btn" onclick = "addStudentAttendance()">Submit</button>
+                    </form>
+                    
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="modal" id="addStudentModal">
+        <div class="small-modal-content modal-animate">
+            <a class="close" onclick="closeAddStudentModal()">&times;</a>
+            <form action="/addStudent/${name}" method="POST">
+                <input type="text" name="studentEmail" id="addStudentInput">
+                <button type="submit" class="btn">Add Student</button>
+            </form>
+        </div>
+    </div>
+    <div class="modal" id="addTeacherModal">
+        <div class="small-modal-content modal-animate">
+            <a class="close" onclick="closeAddTeacherModal()">&times;</a>
+            <h2>Add Teacher</h2>
+            <form action="/addTeacher/${name}" method="POST">
+                <input type="text" name="teacherEmail" id="addTeacherInput">
+                <button type="submit" class="btn">Add Teacher</button>
+            </form>
+        </div>
+    </div>
+
+    </div>`
+}
+
+$(document).ready(function () {
+    $.getJSON('/getCookieDetails', function (user) {
+        $.getJSON('/getClasses', function (classes) {
+            // classesObj = classes
+            // userObj = user
+            let i = 0;
+            while (classes[i] != undefined) {
+                let j = 0;
+                while (j < classes[i].teachers.length) {
+                    if (classes[i].teachers[j].email == user["user"].email) {
+                        let temp = classTemplate(classes[i].name, user['user'].email)
+                        $('#classesSection').append(temp);
+                        break;
+                    }
+                    j++;
+                }
+                i++;
+            }
+        })
+    })
+})
+
+
+let prevRoll = ""
 
 function markAttendance(qrCodeMessage) {
     // console.log(`QR = ${qrCodeMessage}`);
     stdSet.add(qrCodeMessage);
     // console.log(stdSet)
-    document.getElementById('result').innerHTML = '<div style = "margin: 1rem 0rem;" class="result">' + qrCodeMessage + '</div>';
+    let roll = qrCodeMessage.split("%%")
+    if (prevRoll != roll[0]) {
+        prevRoll = roll[0]
+        $('#result').append('<div style = "margin: 1rem 0rem;" class="result">' + roll[0] + '</div>');
+    }
 }
 
 function errorInScanning(errorMessage) {
